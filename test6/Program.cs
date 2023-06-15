3c81f7cdfc979634754429e37b95c7bf7ee3c0eb
Firstivan <Firstivan>
2023-06-15 19:41:53 +0300

    написал третью задачу (6-ую)

---
+++ test6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+string? a = Console.ReadLine();
+
+int aa = Convert.ToInt32(a);
+
+if (aa % 2 == 0)
+{
+   Console.Write("Число является чётным"); 
+}
+
+else
+{
+    Console.Write("Число нечётное!!!!");
+}
