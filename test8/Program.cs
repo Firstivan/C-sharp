0bcb6fa236d9c9d3b9088c8a21c4ae0b8ca7c131
Firstivan <Firstivan>
2023-06-15 19:58:41 +0300

    закончил последнюю задачу 1-го семинара

--- test8/Program.cs
+++ test8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+string? a = Console.ReadLine();
+
+int aa = Convert.ToInt32(a);
+
+for (int i=2; i<=aa; i=i+2)
+{
+   Console.Write(i); 
+}
