61c4eed1582e34f8bddf9026bafca20709679be5
Firstivan <Firstivan>
2023-06-15 19:25:58 +0300

    Составил вторую (4-ую) задачу

--- test4/Program.cs
+++ test4/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+string? a = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string? b = Console.ReadLine();
+
+Console.WriteLine("Введите третье число");
+string? c = Console.ReadLine();
+
+int aa = Convert.ToInt32(a);
+int bb = Convert.ToInt32(b);
+int cc = Convert.ToInt32(c);
+int max = 0;
+
+if (aa > bb)
+{
+    max = aa; 
+}
+
+else
+{
+   max = bb;
+}
+
+if (cc > max)
+{
+    max = cc;
+}
+
+Console.Write("Наибольшее число: ");
+Console.Write(max);
