dc3f8af98808f3798bcf862638c56df9fb83c3c1
Firstivan <Firstivan>
2023-06-16 19:09:06 +0300

    Составил программу задачи №10

---
+++ test10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+//показывает вторую цифру этого числа.
+// 456 -> 5
+
+Console.WriteLine("Put a number from three numbers");
+string? a = Console.ReadLine();
+
+int aa = Convert.ToInt32(a);
+
+int lastDigit = aa % 10;
+int secondDigit = aa /10 % 10;
+int firstDigit = aa / 100;
+
+//Console.WriteLine(firstDigit);
+//Console.WriteLine(secondDigit);
+//Console.WriteLine(lastDigit);
+
+Console.WriteLine($"second number of {aa} is -> {secondDigit}");
+ 
