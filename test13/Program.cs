0a959ad7a5046ef1a056b78cfb530a91e1607483
Firstivan <Firstivan>
2023-06-17 16:29:31 +0300

    Выполнил 13-ую задачу и удалил временную папку 13.1

--- test13/Program.cs
+++ test13/Program.cs
@@ -4,39 +4,18 @@
 // 32679 -> 6
 
 Console.WriteLine("Введите число не менее трёхзначного (от 100 до 999)");
+String? a = Console.ReadLine();
 
-string? a = Console.ReadLine();
+int number = Convert.ToInt32(a);
+int count = a.Length;
 
-int aa = Convert.ToInt32(a);            //Перводим значение строки в число
-// if (99999 > aa > 99)
-// {
-//         Console.WriteLine($"У {aa} -> третьей цифры нет!");
-// }
-
-int Digit1 = aa % 10;                   //Последнее число (слева наравво) из 3-х значного
-int Digit2 = aa /10 % 10;               //Предпоследнее число из 3-х значного
-int Digit3 = aa / 100 % 10;             //Предпредпоследнее число из 3-х значного
-int Digit4 = aa / 1000 % 10;  
-int Digit5 = aa / 10000 % 10;  
-
-string numberString1 = Digit1.ToString();
-string numberString2 = Digit2.ToString();
-string numberString3 = Digit3.ToString();
-string numberString4 = Digit4.ToString();
-string numberString5 = Digit5.ToString();
-
-if (numberString5 = 0)
+if (count < 3)
 {
-  String SumString = numberString1 + numberString2 + numberString3 + numberString4; //Это не изучали.
-  
-        int bb = Convert.ToInt32(SumString);            //Перводим значение строки в число
-
-        int Digit03 = bb / 100% 10; 
-        Console.WriteLine(bb);
-        Console.WriteLine($"{aa} -> {Digit03}");
+    Console.Write("-> третьей цифры нет");
 }
-else
-{
-        String SumString = numberString1 + numberString2 + numberString3 + numberString4  + numberString5; //Это не изучали.
-  
+   
+else 
+{   
+ int result = ((int)(number / Math.Pow(10, count-3))%10);
+Console.WriteLine($"Треья цифра числа {a} -> {result}");
 }
