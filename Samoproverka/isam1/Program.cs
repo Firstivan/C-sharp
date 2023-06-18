0e9c718b0140bb2765a789f2df5efc48884c9133
Firstivan <Firstivan>
2023-06-18 09:04:17 +0300

    Повторил первую задачу второго семинара из примера

---
+++ Samoproverka/isam1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Самопроверка!!!!
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);  // Выводим сообщение пользователю
+    string value = Console.ReadLine(); // Читаем введенную пользователем строку
+    int result = Convert.ToInt32(value); // Преобразуем строку в целое число
+    return result; // Возвращаем полученное значение
+}
+
+int number = Prompt("ВВедите трёхзначное число >");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Введено не корректное число");
+    return;
+}
+Console.WriteLine($"Введенное число: {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра введенного числа {secondRank}");
