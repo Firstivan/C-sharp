8e4e0c179d165b195af1221ca884b16ef22e6454
Firstivan <Firstivan>
2023-06-21 11:26:27 +0300

    Добавил цикл for

---
+++ Samoproverka/FORmetody/Program.cs
@@ -0,0 +1,15 @@
+﻿string text = " Первоночальный текст Валуева имел не точность. И чтобы его заменить потребуются тренировки)))";
+string  Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty; //инициализация строки
+    int length = text.Length;       //число символов в строке
+   for (int i = 0; i <= length; i++)
+   {
+    if(text[i] == oldValue) 
+    result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+   }
+    return result;
+}
+string newText = Replace(text, '-', '+');
+Console.WriteLine(newText);
