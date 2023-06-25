d4af6be8736009eeb7ca0c82f64d3d2f76ba0403
Firstivan <Firstivan>
2023-06-25 22:00:37 +0300

    В самопроверку занёс массив

---
+++ Samoproverka/Massiv/Program.cs
@@ -0,0 +1,10 @@
+﻿int number = 8;
+int [] array = new int [number];
+// int value = array [0];      //
+// array [0] = value;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Random.Shared.Next(0, 2);
+    Console.WriteLine(array[i]);
+}
