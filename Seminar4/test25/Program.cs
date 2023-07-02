9926a179d44dcceb7cdf69a49339c4c962b1380b
Firstivan <Firstivan>
2023-07-02 17:31:47 +0300

    Поправил задачу 25

--- Seminar4/test25/Program.cs
+++ Seminar4/test25/Program.cs
@@ -8,17 +8,18 @@ class Program
 {
     static int ReadInt()
     {
-        Console.WriteLine("Введите целое число!");
+        Console.WriteLine("Введите целое число:");
         int value = Convert.ToInt32(Console.ReadLine());
         return value;
     }
 
+
     static int Power(int A, int B)
     {
         int result = 1;
-        for (int i = 0; i < B; i++)
+        for (int i = 1; i < B; i++)
         {
-            result *= A;
+        result *= A;
         }
         return result;
     }
@@ -27,7 +28,7 @@ class Program
     {
         if (B < 0)
         {
-            Console.WriteLine("Второе число не должно быть меньше нуля!");
+            Console.WriteLine("Второе число не должно быть мельше нуля!");
             return false;
         }
         return true;
@@ -37,8 +38,8 @@ class Program
     {
         int A = ReadInt();
         int B = ReadInt();
-        
-        if (ValidateB(B))
+
+    if (ValidateB(B))
         {
             int result = Power(A, B);
             Console.WriteLine(result);
