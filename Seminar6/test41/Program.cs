0e329b1bd66a6779cd35960015647956f32012a1
Firstivan <Firstivan>
2023-07-02 20:39:44 +0300

    ЗАкончил задачу 41

--- Seminar6/test41/Program.cs
+++ Seminar6/test41/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int count = 0; // переменная для хранения количества чисел больше 0
+
+int PromptInt(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    while (int.TryParse(Console.ReadLine(), out result) == false)
+    {
+        Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+    }
+    return result;
+}
+
+int M = PromptInt("Введите количество чисел:");
+int[] numbers = new int[M];
+
+for (int i = 0; i < M; i++)
+{
+    numbers[i] = PromptInt($"Введите число {i + 1}:");
+    
+    if (numbers[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Количество чисел больше 0: " + count);
