9b1b7abf23f9ebdb8b646fa3d8e404a3127eacd6
Firstivan <Firstivan>
2023-07-03 20:40:55 +0300

    Закончил пример с факториалом и рекурсией

--- Samoproverka/Rekursiya+/SummaOt1DoN/Faktorial/Program.cs
+++ Samoproverka/Rekursiya+/SummaOt1DoN/Faktorial/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Факториал числа (рекурсия)
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *=i;
+    return result;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+
+Console.WriteLine(FactorialFor(5));
+Console.WriteLine(FactorialRec(5));
