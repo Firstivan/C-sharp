cfb61bee6f5ad0bdddfdc2d52f188ee0d33e929f
Firstivan <Firstivan>
2023-07-03 20:58:56 +0300

    Первый пример со степенью готов.

--- Samoproverka/Rekursiya+/aVstepeniN/Program.cs
+++ Samoproverka/Rekursiya+/aVstepeniN/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вычислить a в степени n
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+
+Console.WriteLine(PowerFor(2, 3));
