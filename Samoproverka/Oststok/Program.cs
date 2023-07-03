0e8d68e8ae0a502d15960a6585b88b5d77a89330
Firstivan <Firstivan>
2023-07-03 20:19:04 +0300

    Добавил пример с Рекурсией

---
+++ Samoproverka/Oststok/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Ищем остаток. Введите число"); //Пример 79/10=7 (остаток 9)
+String? aa = Console.ReadLine();
+double number = Convert.ToInt32(aa); //TrueNumber
+
+// double number = TrueNumber / 10;
+// Console.WriteLine("Для начала разделим число на 10 и получим " + number);
+
+double number2 = number % 2;
+Console.WriteLine("Остаток % 2 - " + number2);
+
+double number3 = number % 3;
+Console.WriteLine("Остаток % 3 - " + number3);
+
+double number5 = number % 5;
+Console.WriteLine("Остаток % 5 - " + number5);
+
+double number10 = number % 10;
+Console.WriteLine("Остаток % 10 c переменной double - " + number10);
+
+// int number101 = number % 10;
+// Console.WriteLine("Остаток % 10 c переменной int - " + number101);
+
+double number20 = number % 20;
+Console.WriteLine("Остаток % 20 - " + number20);
+
+double number100 = number % 100;
+Console.WriteLine("Остаток % 100 - " + number100);
---
+++ Samoproverka/Uvelichenie/Program.cs
@@ -0,0 +1,38 @@
+﻿
+//Задача 25: Напишите функцию, которая принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B. НЕ ИСПОЛЬЗОВАТЬ MATH.POW()
+
+int Prompt(string message)                                       //Обозначаем метод для ввода числа
+{
+    System.Console.Write(message);          //Выводим на экран
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse (readInput);    //Конвертирупм введённую с троку в число
+    return result;                                       // потому что не void (возвращаем метод)
+}
+
+int Power (int powerBase, int exponent)          //Задали функцию которая принимает два числа
+{
+    int power = 1;
+    for(int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;               // number = number * A
+    }
+    return power;                 // потому что не void
+ }
+
+bool ValidateExponent (int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше нуля!");
+        return false;
+    }
+    return true;
+}
+
+int powerBase = Prompt("Введите основание: "); 
+int powerExponent = Prompt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно: {Power(powerBase, exponent)}");
+}
