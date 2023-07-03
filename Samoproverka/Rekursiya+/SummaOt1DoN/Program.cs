455f668a059647db78273111337437ef33dd6ccf
Firstivan <Firstivan>
2023-07-03 20:30:13 +0300

    Закончил пример от 1 до n

--- Samoproverka/Rekursiya+/SummaOt1DoN/Program.cs
+++ Samoproverka/Rekursiya+/SummaOt1DoN/Program.cs
@@ -7,5 +7,11 @@ int SumFor(int n)
     return result;
 }
 
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n-1);
+}
 
-Console.WriteLine(result);
+Console.WriteLine(SumFor(11));
+Console.WriteLine(SumRec(10));
