8f2ac240db26bda26cfb029a44e4af89d10ae7ec
Firstivan <Firstivan>
2023-07-03 22:09:59 +0300

    Закончил пример с алфавитом

--- Samoproverka/Rekursiya+/PereborSlov/Program.cs
+++ Samoproverka/Rekursiya+/PereborSlov/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В некотором машинном алфавите имеются четыре буквы "а", "и", "с" и "в". Покажите все слова, состоящие
+// из Т букв, которые можно построите из букв этого алфавита.
+
+int n = 1;  // счётчик
+
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[2]);
