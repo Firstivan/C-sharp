a7cfa5193b79a0e5bd05758505b6cc42c4043724
Firstivan <Firstivan>
2023-07-07 08:50:29 +0300

    Поправил МакМин

--- Samoproverka/MaxMin/Program.cs
+++ Samoproverka/MaxMin/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int a, int b, int c)
+{
+    int res = a;
+    if(b > res) res = b;
+    if(c > res) res = c;
+    return res;
+}
+
+Console.WriteLine("Введите первое число:");
+int numberInt1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberInt2 = Convert.ToInt32(Console.ReadLine());;
+Console.WriteLine("Введите третье число:");
+int numberInt3 = Convert.ToInt32(Console.ReadLine());;
+Console.WriteLine($"максимальное число: {Max(numberInt1, numberInt2, numberInt3)}");
