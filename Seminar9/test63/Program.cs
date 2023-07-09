ed05dbe75e014483127211d6bf08e07795b1f2bd
Firstivan <Firstivan>
2023-07-10 00:18:52 +0300

    Добавил задачу 65

---
+++ Seminar9/test63/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 63. Задайте значение N. Напишите программу, которая выдаёт все натуральные  числа от 1 до N.
+
+int number = 10;
+
+void Perechislenie(int LastNumber)
+{
+    if (LastNumber <= 0) return;
+    Perechislenie(LastNumber - 1);
+    Console.Write(LastNumber + "");
+}
+Perechislenie(number);
---
+++ Seminar9/test65/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 65. Задайте значение M и N. Напишите программу, которая выдаёт все натуральные  числа от M до N.
+
+int numberM = 1;
+int numberN = 4;
+
+void Perechislenie(int LastNumber, int FirstNumber)
+{
+    if (LastNumber <= FirstNumber -1) return;
+    Perechislenie(LastNumber - 1, FirstNumber);
+    Console.Write(LastNumber + "");
+}
+Perechislenie(numberN, numberM);
