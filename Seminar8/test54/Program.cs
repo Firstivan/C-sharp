97ab67649ced625c1dc352376e86740f87ac4e63
Firstivan <Firstivan>
2023-07-10 22:41:08 +0300

    Задача №54 не решена

--- Seminar8/test54/Program.cs
+++ Seminar8/test54/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
+// каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
@@ -8,3 +9,47 @@
 // 9 5 3 2
 // 8 4 4 2
 
+// const int Rows = 3;
+// const int Columns = 4;
+// int[,] GetRandomMatrix(int row, int columns)
+// {
+// int[,] matrix = new int[row, columns];   
+// for(int i = 0; i < matrix.GetLength(0); i++)   //i stroli     j stolbtsy
+// {
+//     for(int j = 0; j < matrix.GetLength(1); j++)    //гет лент - чисто строка
+//     {
+//         matrix[i, j] = Random.Shared.Next(1, 10);
+//     }
+// }
+// return matrix;
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+// for(int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for(int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write($"{matrix[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+// }
+
+// void SwapFirstAndLastRows(int[,] array)
+// {
+//     int rowCount = array.GetLength(0);
+//     int columnCount = array.GetLength(1);
+//     for(int j = 0; j < columnCount; j++)
+//     {
+//         int temp = array[0, j];
+//             array[0, j] = array[rowCount - 1, j];
+//             array[rowCount - 1, j] = temp;
+//     }
+// }
+
+// int[,] myMatrix = GetRandomMatrix(Rows, Columns);
+// PrintMatrix(myMatrix);
+// SwapFirstAndLastRows(myMatrix);
+// Console.WriteLine();
+// PrintMatrix(myMatrix);
