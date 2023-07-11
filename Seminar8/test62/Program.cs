d7d1dea90c6127677df6a3553224fddf52706a0f
Firstivan <Firstivan>
2023-07-12 00:34:08 +0300

    Задачу 62 не доделал.

--- Seminar8/test62/Program.cs
+++ Seminar8/test62/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] GenerateArray()
+{
+    int rows = 4;
+    int columns = 4;
+    int[,] array = new int[rows, columns];
+    Random random = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(10, 100); // Генерация произвольного двузначного числа (10-99)
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+    int numDigits = Math.Max(rows, columns).ToString().Length + 1;
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j].ToString().PadLeft(numDigits, ' '));
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] originalArray = GenerateArray();
+
+Console.WriteLine("Исходный массив:");
+PrintArray(originalArray);
+Console.WriteLine();
+
+int[,] spiralMatrix = GetSpiral();
+
+Console.WriteLine("Спиральная матрица:");
+PrintArray(spiralMatrix);
