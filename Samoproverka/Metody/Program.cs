e30a6a2c162b2123504f2c0766902df9daa305f7
Firstivan <Firstivan>
2023-07-23 00:13:23 +0300

    Повторяю методы и строки

--- Samoproverka/Metody/Program.cs
+++ Samoproverka/Metody/Program.cs
@@ -1,64 +1,80 @@
 ﻿//Разные методы.
 
-void Method1()
+// void Method1()
+// {
+// Console.WriteLine("Метод 1 !");
+// }
+
+// Method1(); //Вызывыем Метод 1
+//______________________________________
+
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg: " Вызвали Метод 2."); //Вызывыем Метод 2
+//______________________________________
+
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21("Вызвали Метод21 4 раза)))  ", 4); //Вызывыем Метод 21
+//______________________________________
+
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int a = Method3();
+// Console.WriteLine(a);   //Вызывыем Метод 3
+//______________________________________
+
+
+// string Method4(int count, string input)
+// {
+//     int i = 0;
+//     string result = string.Empty;
+//     while (i < count)
+//     {
+//         result = result + input;
+//         i++;
+//     }
+//     return result; 
+// }
+// string res = Method4(5, "Текст из Метода 4.  ");
+// Console.WriteLine(res); //Вызывыем Метод 4
+//______________________________________
+
+
+// string Method5(int count, string input)
+// {
+//    string result = string.Empty;
+//    for (int i = 0; i < count; i++)
+//    {
+//         result = result + input;
+//    }
+//    return result; 
+// }
+
+// string res = Method5(5, "Текст из Метода 5.  ");
+// Console.WriteLine(res); //Вызывыем Метод 5
+//______________________________________
+//______________________________________
+
+// проверка метода
+
+void ReadInt(String message)
 {
-Console.WriteLine("Метод 1 !");
+    Console.WriteLine(message);
 }
-
-Method1(); //Вызывыем Метод 1
-
-
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-Method2(msg: " Вызвали Метод 2."); //Вызывыем Метод 2
-
-
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
-Method21("Вызвали Метод21 4 раза)))  ", 4); //Вызывыем Метод 21
-
-
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-int a = Method3();
-Console.WriteLine(a);   //Вызывыем Метод 3
-
-
-string Method4(int count, string input)
-{
-    int i = 0;
-    string result = string.Empty;
-    while (i < count)
-    {
-        result = result + input;
-        i++;
-    }
-    return result; 
-}
-string res = Method4(5, "Текст из Метода 4.  ");
-Console.WriteLine(res); //Вызывыем Метод 4
-
-
-string Method5(int count, string input)
-{
-   string result = string.Empty;
-   for (int i = 0; i < count; i++)
-   {
-        result = result + input;
-   }
-   return result; 
-}
-
-string res = Method5(5, "Текст из Метода 5.  ");
-Console.WriteLine(res); //Вызывыем Метод 5
+ReadInt(message: " Вызвали Метод 1."); //Вызывыем Метод 2
+ReadInt(message: " Вызвали Метод 2."); //Вызывыем Метод 2
